1b5cdb61d7346c348d0bb1bbed514111784f5272
Vivek <vivekmunde@gmail.com>
2019-06-02 14:11:02 +0530

    initial app component setup

---
+++ imports/startup/client/index.js
@@ -0,0 +1,8 @@
+import { Meteor } from 'meteor/meteor';
+import React from 'react';
+import { render } from 'react-dom';
+import App from '../../ui/App';
+
+Meteor.startup(() => {
+    render(<App />, document.getElementById('app'));
+});
